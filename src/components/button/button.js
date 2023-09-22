b10daa1895cbed9a1ee91fc83061d08937da9901
mac <mac@MacBook-Air-de-mac.local>
2023-09-22 03:18:58 +0100

    add button component

---
+++ src/components/button/button.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles/button.css";
+
+export default function Button({
+  children,
+  type = "primary",
+  onClick = () => {},
+}) {
+  return (
+    <button
+      className={`btn ${type === "primary" ? "btn-primary" : "btn-secondary"}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
