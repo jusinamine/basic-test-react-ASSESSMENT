8629ccc488053bd4b08cf84a18afa3694c40f88a
mac <mac@MacBook-Air-de-mac.local>
2023-09-22 14:14:03 +0100

    complete search results

--- src/components/input/Search.js
+++ src/components/input/Search.js
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { SearchIcon } from "../icon";
 import "./styles/search.css";
-export default function Search({ show = false }) {
+export default function Search({
+  show = false,
+  handleSearch = () => {},
+  results,
+  onClose = () => {},
+  openSearchRef = null,
+  onSelect = () => {},
+}) {
+  const searchRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        searchRef.current &&
+        !searchRef.current.contains(event.target) &&
+        !openSearchRef.current.contains(event.target)
+      ) {
+        onClose();
+      }
+    };
+
+    if (show) {
+      document.addEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [show]);
+
   return (
     <>
       {show && (
         <div className="absolute-blur-box">
-          <div>
-            <div className="search-input-box">
-              <SearchIcon />
-              <input className="search-input" />
+          <div ref={searchRef}>
+            <div>
+              <div className="search-input-box">
+                <SearchIcon />
+                <input
+                  onChange={(e) => {
+                    handleSearch(e.target.value);
+                  }}
+                  className="search-input"
+                />
+              </div>
+            </div>
+            <div className="result-box">
+              {results?.map((item, index) => (
+                <div
+                  key={"search-item" + index}
+                  onClick={(e) => {
+                    onSelect(e, item);
+                    onClose();
+                  }}
+                  className="result-item"
+                >
+                  {item}
+                </div>
+              ))}
             </div>
           </div>
-          <div className="result-box">result</div>
         </div>
       )}
     </>
